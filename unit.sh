171ab74cfd3bc2715b233691cc068ef8b07f54e7
Peterkemley <peterkemley@hotmail.com>
2021-11-17 23:29:43 +0000

    small change

--- unit.sh
+++ unit.sh
@@ -3,10 +3,13 @@ javac Dec2Hex.java
 #Unit test 1 will be testing if the arguments contain numbers above 15.
 echo "Unit Test 1: Testing for numbers above 15"
 echo "Dec2Hex Program Starting"
+
 java Dec2Hex 16 > test1.txt
+
 test1=$(cat test1.txt)
+
 echo "$test1"
-        if [[$test1 == *"Hexadecimal"* ]]; then
+        if [$test1 == *"Hexadecimal"* ]; then
         echo"Test1 Passed"
         else
         echo"Test1 Failed"
